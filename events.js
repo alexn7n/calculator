81fb055df39f9db87617c93b970960d454ee5f98
alexn7n <156884962+alexn7n@users.noreply.github.com>
2025-03-22 12:12:15 +1100

    add operator logic

--- events.js
+++ events.js
@@ -1,52 +1,72 @@
-
-
 // Variables
-let numberA = 0;
-let numberB = 0;
-let operator = "/";
+let numberA = null;
+let numberB = null;
+let operator = null;
+let result = 0;
 
 // Dom elements
 
 const numberButtonElements = document.querySelectorAll(".number-button");
+const calculateButtonElements = document.querySelectorAll(".calculate-button");
 
-const plusButtonElement = document.querySelector("plus");
-const subtractButtonElement = document.querySelector("subtract");
-const multiplyButtonElement = document.querySelector("multiply");
-const equalsButtonElement = document.querySelector("equals");
-const clearButtonElement = document.querySelector("clear");
+const equalsButtonElement = document.querySelector("#equals-button");
+const clearButtonElement = document.querySelector("#clear-button");
 
 let displayElement = document.querySelector("#display");
 
-
 function add(a, b) {
-    return a + b;
+  return a + b;
 }
 
 function subtract(a, b) {
-    return a - b;
+  return a - b;
 }
 
 function multiply(a, b) {
-    return a * b;
+  return a * b;
 }
 
 function divide(a, b) {
-    return a / b;
+  return a / b;
 }
 
 function operate(numberA, numberB, operator) {
-    (operator === "/") ? divide(numberA, numberB) :
-    (operator === "*")? multiply(numberA, numberB) :
-    (operator === "-") ? subtract(numberA, numberB) :
-    (operator = "+")? add(numberA, numberB) :
-    "Error";    
+  switch (operator) {
+    case "/":
+      return divide(numberA, numberB);
+    case "*":
+      return multiply(numberA, numberB);
+    case "-":
+      return subtract(numberA, numberB);
+    case "+":
+      return add(numberA, numberB);
+    default:
+      return "Error";
+  }
 }
 
-
-  numberButtonElements.forEach(button => {
-    button.addEventListener("click", () => populateDisplay(button));
+numberButtonElements.forEach((button) => {
+  button.addEventListener("click", () => populateDisplay(button));
 });
 
 function populateDisplay(button) {
-    displayElement.textContent = button.textContent;
+  displayElement.textContent += button.textContent;
 }
+
+calculateButtonElements.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (numberA === null) {
+      numberA = displayElement.textContent;
+    } else {
+      numberB = displayElement.textContent;
+    }
+    displayElement.textContent = "";
+    operator = button.textContent;
+  });
+});
+
+equalsButtonElement.addEventListener("click", () => {
+  const result = operate(Number(numberA), Number(numberB), operator);
+  displayElement.textContent = result;
+  console.log(result);
+});
