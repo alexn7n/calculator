424f8d91eac112b7ad1592068adc23e7a1b23b4b
alexn7n <156884962+alexn7n@users.noreply.github.com>
2025-03-31 18:14:05 +1100

    update calculation logic

--- events.js
+++ events.js
@@ -1,18 +1,21 @@
-// Variables
-let numberA = null;
-let numberB = null;
-let operator = null;
-let result = 0;
-
 // Dom elements
 
 const numberButtonElements = document.querySelectorAll(".number-button");
-const calculateButtonElements = document.querySelectorAll(".calculate-button");
-
+const operatorButtonElements = document.querySelectorAll(".operate-button");
 const equalsButtonElement = document.querySelector("#equals-button");
 const clearButtonElement = document.querySelector("#clear-button");
+const previousOperandElement = document.querySelector("#previous-operand");
+const currentOperandElement = document.querySelector("#current-operand");
 
-let displayElement = document.querySelector("#display");
+// Values object
+
+const values = {
+  currentOperand: currentOperandElement,
+  previousOperand: previousOperandElement,
+  operation: undefined,
+};
+
+// Math functions
 
 function add(a, b) {
   return a + b;
@@ -30,43 +33,72 @@ function divide(a, b) {
   return a / b;
 }
 
-function operate(numberA, numberB, operator) {
-  switch (operator) {
-    case "/":
-      return divide(numberA, numberB);
-    case "*":
-      return multiply(numberA, numberB);
-    case "-":
-      return subtract(numberA, numberB);
-    case "+":
-      return add(numberA, numberB);
-    default:
-      return "Error";
-  }
+// Calculator functions
+
+function clear() {
+  currentOperandElement.textContent = "";
+  previousOperandElement.textContent = "";
+  values.operation = undefined;
 }
+
+function remove() {
+
+}
+
+function appendNumber(number) {
+if (number === "." && values.currentOperand.textContent.includes(".")) return
+  values.currentOperand.textContent += number;
+}
+
+function chooseOperation(op) {
+  if (values.currentOperand.textContent === "") return
+  if (values.previousOperand.textContent !== "") {
+    compute(values.previousOperand.textContent, values.currentOperand.textContent, values.operation);
+  }
+  values.operation = op;
+  values.previousOperand.textContent = values.currentOperand.textContent;
+  values.currentOperand.textContent = "";
+}
+
+function compute(numberA, numberB, operator) {
+  let result;
+  switch (operator) {
+      case "/": return divide(numberA, numberB);
+      case "*": return multiply(numberA, numberB);
+      case "-": return subtract(numberA, numberB);
+      case "+": return add(numberA, numberB);
+      default: result = "Error";
+  }
+values.currentOperand.textContent = result;
+values.previousOperand.textContent = "";
+values.operation = undefined;
+}
+
+function updateDisplay() {
+  currentOperandElement.textContent = values.currentOperand.textContent;  
+}
+
+// Click events
 
 numberButtonElements.forEach((button) => {
-  button.addEventListener("click", () => populateDisplay(button));
-});
-
-function populateDisplay(button) {
-  displayElement.textContent += button.textContent;
-}
-
-calculateButtonElements.forEach((button) => {
   button.addEventListener("click", () => {
-    if (numberA === null) {
-      numberA = displayElement.textContent;
-    } else {
-      numberB = displayElement.textContent;
-    }
-    displayElement.textContent = "";
-    operator = button.textContent;
+    appendNumber(button.textContent);
+    updateDisplay();
   });
 });
 
-equalsButtonElement.addEventListener("click", () => {
-  const result = operate(Number(numberA), Number(numberB), operator);
-  displayElement.textContent = result;
-  console.log(result);
+operatorButtonElements.forEach((button) => {
+  button.addEventListener("click", () => {
+    chooseOperation(button.textContent);
+    updateDisplay();
+  });
 });
+
+equalsButtonElement.addEventListener ('click', button => {
+  compute(values.currentOperand.textContent, values.previousOperand.textContent, values.operation)
+  updateDisplay();
+})
+
+clearButtonElement.addEventListener ('click', () => {
+  clear()
+})
